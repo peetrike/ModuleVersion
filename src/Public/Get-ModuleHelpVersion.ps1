b7e2ed708c42d284eac11038702221d198123054
Peter Wawa <meelisn@outlook.com>
2023-10-13 15:17:06 +0300

    add new function

---
+++ src/Public/Get-ModuleHelpVersion.ps1
@@ -0,0 +1,37 @@
+﻿function Get-ModuleHelpVersion {
+    [CmdletBinding()]
+    param (
+            [string[]]
+        $Module
+    )
+
+    $HelpInfoNamespace = @{ helpInfo = 'http://schemas.microsoft.com/powershell/help/2010/05' }
+
+    $ModuleParams = @{
+        ListAvailable = $true
+    }
+    if ($Module) {
+        $ModuleParams.Name = $Module
+    }
+
+    $Modules = Get-Module @ModuleParams | Where-Object HelpInfoUri
+
+    foreach ($mModule in $Modules) {
+        $mDir = $mModule.ModuleBase
+
+        if (Test-Path $mdir\*helpinfo.xml) {
+            $mNodes = Get-ChildItem -Path $mdir\*helpinfo.xml -ErrorAction SilentlyContinue |
+                Select-Xml -Namespace $HelpInfoNamespace -XPath '//helpInfo:UICulture'
+            foreach ($mNode in $mNodes) {
+                $mCulture = $mNode.Node.UICultureName
+                $mVer = $mNode.Node.UICultureVersion
+
+                [PSCustomObject]@{
+                    ModuleName = $mModule.Name
+                    Culture    = $mCulture
+                    Version    = $mVer
+                }
+            }
+        }
+    }
+}
